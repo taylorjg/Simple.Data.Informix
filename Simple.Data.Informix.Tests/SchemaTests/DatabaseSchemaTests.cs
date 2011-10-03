79fcd62e86600f8a9e596b4cc53304145f09f287
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2011-10-03 21:14:35 +0100

    Changed unit tests to work against the stores demo database

--- Simple.Data.Informix.Tests/SchemaTests/DatabaseSchemaTests.cs
+++ Simple.Data.Informix.Tests/SchemaTests/DatabaseSchemaTests.cs
@@ -44,66 +44,76 @@ namespace Simple.Data.Informix.Tests.SchemaTests
         [Test]
         public void TestTables()
         {
-            Assert.AreEqual(1, Schema.Tables.Count(t => t.ActualName == "name"));
+            Assert.AreEqual(1, Schema.Tables.Count(t => t.ActualName == "stock"));
         }
 
         [Test]
         public void TestColumns()
         {
-            var table = Schema.FindTable("name");
-            Assert.AreEqual(1, table.Columns.Count(c => c.ActualName == "rec_num"));
+            var table = Schema.FindTable("stock");
+            Assert.AreEqual(1, table.Columns.Count(c => c.ActualName == "unit_price"));
         }
 
         [Test]
         public void TestPrimaryKey()
         {
-            var table = Schema.FindTable("name");
+            var table = Schema.FindTable("customer");
             Assert.AreEqual(1, table.PrimaryKey.Length);
-            Assert.AreEqual("rec_num", table.PrimaryKey[0]);
+            Assert.AreEqual("customer_num", table.PrimaryKey[0]);
         }
 
         [Test]
         public void TestCompoundPrimaryKey()
         {
-            var table = Schema.FindTable("accounts");
+            var table = Schema.FindTable("stock");
             Assert.AreEqual(2, table.PrimaryKey.Length);
-            Assert.AreEqual("acc_num", table.PrimaryKey[0]);
-            Assert.AreEqual("acc_type", table.PrimaryKey[1]);
+            Assert.AreEqual("stock_num", table.PrimaryKey[0]);
+            Assert.AreEqual("manu_code", table.PrimaryKey[1]);
         }
 
         [Test]
         public void TestForeignKey()
         {
-            var table = Schema.FindTable("child");
+            var table = Schema.FindTable("stock");
             var fkey = table.ForeignKeys.Single();
-            Assert.AreEqual("rec_num", fkey.Columns[0]);
-            Assert.AreEqual("name", fkey.MasterTable.Name);
-            Assert.AreEqual("rec_num", fkey.UniqueColumns[0]);
+            Assert.AreEqual("stock", fkey.DetailTable.Name);
+            Assert.AreEqual("manu_code", fkey.Columns[0]);
+            Assert.AreEqual("manufact", fkey.MasterTable.Name);
+            Assert.AreEqual("manu_code", fkey.UniqueColumns[0]);
         }
 
         [Test]
         public void TestCompoundForeignKey()
         {
-            var table = Schema.FindTable("sub_accounts");
-            var fkey = table.ForeignKeys.Single();
-            Assert.AreEqual("ref_num", fkey.Columns[0]);
-            Assert.AreEqual("ref_type", fkey.Columns[1]);
-            Assert.AreEqual("accounts", fkey.MasterTable.Name);
-            Assert.AreEqual("acc_num", fkey.UniqueColumns[0]);
-            Assert.AreEqual("acc_type", fkey.UniqueColumns[1]);
+            var table = Schema.FindTable("items");
+            var fkeys = table.ForeignKeys;
+
+            Assert.AreEqual(2, fkeys.Count);
+
+            Assert.AreEqual("items", fkeys[0].DetailTable.Name);
+            Assert.AreEqual("order_num", fkeys[0].Columns[0]);
+            Assert.AreEqual("orders", fkeys[0].MasterTable.Name);
+            Assert.AreEqual("order_num", fkeys[0].UniqueColumns[0]);
+
+            Assert.AreEqual("items", fkeys[1].DetailTable.Name);
+            Assert.AreEqual("stock_num", fkeys[1].Columns[0]);
+            Assert.AreEqual("manu_code", fkeys[1].Columns[1]);
+            Assert.AreEqual("stock", fkeys[1].MasterTable.Name);
+            Assert.AreEqual("stock_num", fkeys[1].UniqueColumns[0]);
+            Assert.AreEqual("manu_code", fkeys[1].UniqueColumns[1]);
         }
 
         [Test]
         public void TestIdentityIsTrueWhenItShouldBe()
         {
-            var column = Schema.FindTable("name").FindColumn("rec_num");
+            var column = Schema.FindTable("customer").FindColumn("customer_num");
             Assert.IsTrue(column.IsIdentity);
         }
 
         [Test]
         public void TestIdentityIsFalseWhenItShouldBe()
         {
-            var column = Schema.FindTable("accounts").FindColumn("acc_num");
+            var column = Schema.FindTable("manufact").FindColumn("manu_code");
             Assert.IsFalse(column.IsIdentity);
         }
 
